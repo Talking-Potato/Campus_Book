98c67be6a6792e79ba7a93e86458faa8a024cd77
masibasi <jimin_lee98@naver.com>
2022-05-26 23:50:31 +0900

    경험치 오르는 애니메이션, 레벨업 구현

--- app/src/main/java/com/example/term_project/MyLevelFragment.java
+++ app/src/main/java/com/example/term_project/MyLevelFragment.java
@@ -1,21 +1,21 @@
 package com.example.term_project;
 
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyLevelFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyLevelFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -23,9 +23,7 @@ public class MyLevelFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Context context;
     ProgressBar expBar;
     Button addBtn, lowerBtn;
     private int expVal = 0;
@@ -36,35 +34,19 @@ public class MyLevelFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Mylevel.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyLevelFragment newInstance(String param1, String param2) {
-        MyLevelFragment fragment = new MyLevelFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup ) inflater.inflate(R.layout.fragment_mylevel, container, false);
+        context = container.getContext();
 
         expBar = (ProgressBar) rootView.findViewById(R.id.expBar);
         addBtn = (Button) rootView.findViewById(R.id.addExp);
@@ -75,10 +57,28 @@ public class MyLevelFragment extends Fragment {
             public void onClick(View view) {
                 expVal = expBar.getProgress();
 //                Log.d("EXP","EXP progresas " + expVal);
-                if(expVal < 120){
-                    expVal = expVal + 10;
+                int earnExp = 10;
+                if(expVal < expBar.getMax()){
+                    for(int i = 0; i < earnExp; i++) {
+                        Handler mHandler = new Handler();
+                        mHandler.postDelayed(new Runnable() {
+                            public void run() {
+                                expVal = expVal + 1;
+                                expBar.setProgress(expVal);
+                                if (expVal >= expBar.getMax()) {
+                                    expVal = 0;
+                                    Toast.makeText(context, "LEVELUP", Toast.LENGTH_SHORT).show();
+                                    expBar.setProgress(expVal);
+                                }
+                            }
+                        }, i * 10); //
+
+                    }
+
                 }
                 expBar.setProgress(expVal);
+
+
             }
         });
 
