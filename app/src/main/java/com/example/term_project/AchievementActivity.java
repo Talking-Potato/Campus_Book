9bbc472b4374c0071106e4ca28c589f71b45bf35
Hyunwoo <woo980711@naver.com>
2022-05-27 03:38:55 +0900

    DB에서 받아서 업적 목록 출력을 위한 ListView 생성

--- app/src/main/java/com/example/term_project/AchievementActivity.java
+++ app/src/main/java/com/example/term_project/AchievementActivity.java
@@ -1,14 +1,46 @@
 package com.example.term_project;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class AchievementActivity extends AppCompatActivity {
+
+    ArrayList<Achievement> achievList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
+
+        this.IntializeData();
+
+        ListView listView = (ListView)findViewById(R.id.achivList);
+        final AchievementAdapter myAdapter = new AchievementAdapter(this,achievList);
+
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                Toast.makeText(getApplicationContext(),
+                        myAdapter.getItem(position).getTitle(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void IntializeData()
+    {
+        achievList = new ArrayList<Achievement>();
+
+        achievList.add(new Achievement("매장/건물 최초 방문 (일 최대 각 1회 / gps 거리 10m이내 20분 이상)", "22.05.26"));
+        achievList.add(new Achievement("매장/건물 10회 방문+20", "22.05.27"));
     }
 }
