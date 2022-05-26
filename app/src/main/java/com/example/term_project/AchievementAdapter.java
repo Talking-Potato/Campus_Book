936aca91b1b89b80ce77cef3db56318a65db7215
Hyunwoo <woo980711@naver.com>
2022-05-27 03:38:55 +0900

    DB에서 받아서 업적 목록 출력을 위한 ListView 생성

---
+++ app/src/main/java/com/example/term_project/AchievementAdapter.java
@@ -0,0 +1,50 @@
+package com.example.term_project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AchievementAdapter extends BaseAdapter {
+    Context mContext = null;
+    LayoutInflater mLayoutInflater = null;
+    ArrayList<Achievement> list;
+
+    public AchievementAdapter(Context context, ArrayList<Achievement> data) {
+        mContext = context;
+        list = data;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Achievement getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = mLayoutInflater.inflate(R.layout.listview_achiev, null);
+
+        TextView title = (TextView) view.findViewById(R.id.achivTitle);
+        TextView date = (TextView) view.findViewById(R.id.achivDate);
+
+        title.setText(list.get(position).getTitle());
+        date.setText(list.get(position).getDate());
+
+        return view;
+    }
+}
